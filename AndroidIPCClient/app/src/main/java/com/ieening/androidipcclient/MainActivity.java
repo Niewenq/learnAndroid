e3459e18559e43dab5e75895c1d13c448f803c27
niewenqing <nie_wq@163.com>
2024-01-07 20:41:58 +0800

    init AndroidIPCClient project

--- AndroidIPCClient/app/src/main/java/com/ieening/androidipcclient/MainActivity.java
+++ AndroidIPCClient/app/src/main/java/com/ieening/androidipcclient/MainActivity.java
@@ -2,26 +2,13 @@ package com.ieening.androidipcclient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-import com.ieening.androidipcclient.databinding.ActivityMainBinding;
-
 public class MainActivity extends AppCompatActivity {
-    private static final String serverPackageName = "com.ieening.androidipcserver";
-    private static final String serverActivityName = "com.ieening.androidipcserver.MainActivity";
-    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-//        Intent intent = new Intent();
-//        intent.setClassName(serverPackageName, serverActivityName);
-//        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//        intent.putExtra("serviceName", "MyService");
-//        startActivity(intent);
+        setContentView(R.layout.activity_main);
     }
 }
